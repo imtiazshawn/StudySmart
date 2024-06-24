2d0ae8f79d73605142b076900b0b7fc8ef1d4869
Imtiaz Shawn <imtiazshawn01@gmail.com>
2024-06-24 22:26:22 +0600

    first commit

---
+++ app/src/main/java/com/example/studysmart/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.studysmart
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.studysmart.dashboard.DashboardScreen
+import com.example.studysmart.ui.theme.StudySmartTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            StudySmartTheme {
+                DashboardScreen()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/studysmart/dashboard/DashboardScreen.kt
@@ -0,0 +1,304 @@
+package com.example.studysmart.dashboard
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.example.studysmart.R
+import com.example.studysmart.components.AddSubjectDialog
+import com.example.studysmart.components.CountCard
+import com.example.studysmart.components.SubjectCard
+import com.example.studysmart.components.studySessionsList
+import com.example.studysmart.components.tasksList
+import com.example.studysmart.domain.model.Session
+import com.example.studysmart.domain.model.Subject
+import com.example.studysmart.domain.model.Task
+
+@Composable
+fun DashboardScreen() {
+    val subjects = listOf(
+        Subject(name = "English", goalHours = 10f, colors = Subject.subjectCardColors[0], subjectId = 0),
+        Subject(name = "Physics", goalHours = 10f, colors = Subject.subjectCardColors[1], subjectId = 0),
+        Subject(name = "Maths", goalHours = 10f, colors = Subject.subjectCardColors[2], subjectId = 0),
+        Subject(name = "Geology", goalHours = 10f, colors = Subject.subjectCardColors[3], subjectId = 0),
+        Subject(name = "Fine Arts", goalHours = 10f, colors = Subject.subjectCardColors[4], subjectId = 0),
+    )
+
+    val tasks = listOf(
+        Task(
+            title = "Prepare Notes",
+            description = "",
+            dueDate = 0L,
+            priority = 0,
+            relatedToSubject = "",
+            isComplete = false,
+            taskSubjectId = 0,
+            taskId = 1
+        ),
+        Task(
+            title = "Do Homework",
+            description = "",
+            dueDate = 0L,
+            priority = 2,
+            relatedToSubject = "",
+            isComplete = true,
+            taskSubjectId = 0,
+            taskId = 1
+        ),
+        Task(
+            title = "Go Coaching",
+            description = "",
+            dueDate = 0L,
+            priority = 1,
+            relatedToSubject = "",
+            isComplete = false,
+            taskSubjectId = 0,
+            taskId = 1
+        ),
+        Task(
+            title = "Assignment",
+            description = "",
+            dueDate = 0L,
+            priority = 0,
+            relatedToSubject = "",
+            isComplete = false,
+            taskSubjectId = 0,
+            taskId = 1
+        ),
+    )
+
+    val sessions = listOf(
+        Session (
+            relatedToSubject = "English",
+            date = 0L,
+            duration = 2,
+            sessionSubjectId = 0,
+            sessionId = 0
+        ),
+        Session (
+            relatedToSubject = "Physics",
+            date = 0L,
+            duration = 2,
+            sessionSubjectId = 0,
+            sessionId = 0
+        ),
+        Session (
+            relatedToSubject = "Maths",
+            date = 0L,
+            duration = 2,
+            sessionSubjectId = 0,
+            sessionId = 0
+        ),
+        Session (
+            relatedToSubject = "Geology",
+            date = 0L,
+            duration = 2,
+            sessionSubjectId = 0,
+            sessionId = 0
+        ),
+    )
+
+    var isAddSubjectDialogOpen by rememberSaveable {
+        mutableStateOf(false)
+    }
+    var subjectName by remember { mutableStateOf("") }
+    var goalHours by remember { mutableStateOf("") }
+    var selectedColor by remember { mutableStateOf(Subject.subjectCardColors.random()) }
+    AddSubjectDialog(
+        isOpen = isAddSubjectDialogOpen,
+        subjectName = subjectName,
+        goalHours = goalHours,
+        onSubjectNameChange = {subjectName = it},
+        onGoalHoursChange = {goalHours = it},
+        selectedColors = selectedColor,
+        onColorChange = { selectedColor = it },
+        onDismissRequest = { isAddSubjectDialogOpen = false },
+        onConfirmButtonClick = {isAddSubjectDialogOpen = false}
+    )
+
+    Scaffold(
+        topBar = { DashboardScreenTopBar() }
+    ) { paddingValues ->
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            item {
+                CountCardsSection(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(12.dp),
+                    subjectCount = 5,
+                    studiedHours = "10",
+                    goalHours = "15"
+                )
+            }
+            item {
+                SubjectCardsSection(
+                    modifier = Modifier.fillMaxWidth(),
+                    subjectList = subjects,
+                    onAddIconClick = {
+                        isAddSubjectDialogOpen = true
+                    }
+                )
+            }
+            item {
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 48.dp, vertical = 20.dp)
+                ) {
+                    Text(text = "Start Study Session")
+                }
+            }
+            tasksList(
+                sectionTitle = "UPCOMING TASKS",
+                emptyListText = "You don't have any upcoming Tasks. \n" +
+                        " Click the + button in subjects screen to add new task.",
+                tasks = tasks,
+                onTaskCardClick = {},
+                onCheckBoxClick = {}
+            )
+            item {
+                Spacer(modifier = Modifier.height(20.dp))
+            }
+            studySessionsList(
+                sectionTitle = "RECENT STUDY SESSIONS",
+                emptyListText = "You don't have any recent study sessions. \n" +
+                        "Start a study session to begin recording your progress",
+                sessions = sessions,
+                onDeleteIconClick = {}
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun DashboardScreenTopBar() {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(text = "StudySmart", style = MaterialTheme.typography.headlineMedium)
+        }
+    )
+}
+
+@Composable
+private fun CountCardsSection(
+    modifier: Modifier,
+    subjectCount: Int,
+    studiedHours: String,
+    goalHours: String
+) {
+    Row(modifier = modifier) {
+        CountCard(
+            modifier = Modifier.weight(1f),
+            headingText = "Subject Count",
+            count = "$subjectCount"
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        CountCard(
+            modifier = Modifier.weight(1f),
+            headingText = "Studying Hours",
+            count = studiedHours
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        CountCard(
+            modifier = Modifier.weight(1f),
+            headingText = "Goal Study Hours",
+            count = goalHours
+        )
+    }
+}
+
+@Composable
+private fun SubjectCardsSection(
+    modifier: Modifier,
+    subjectList: List<Subject>,
+    emptyListText: String = "You don't have any subjects. \n Click the + button to add new subject.",
+    onAddIconClick: () -> Unit
+) {
+    Column (modifier = modifier) {
+        Row (
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "SUBJECTS",
+                style = MaterialTheme.typography.bodySmall,
+                modifier = Modifier.padding(start = 12.dp)
+            )
+            IconButton(onClick = onAddIconClick) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add Subject"
+                )
+            }
+        }
+        if(subjectList.isEmpty()) {
+            Image(
+                painter = painterResource(id = R.drawable.img_books),
+                modifier = Modifier
+                    .size(120.dp)
+                    .align(Alignment.CenterHorizontally),
+                contentDescription = emptyListText
+            )
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = emptyListText,
+                style = MaterialTheme.typography.bodySmall,
+                color = Color.Gray,
+                textAlign = TextAlign.Center
+            )
+        }
+        LazyRow(
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            contentPadding = PaddingValues(start = 12.dp, end = 12.dp)
+        ) {
+            items(subjectList) {
+                subject ->
+                SubjectCard(
+                    subjectName = subject.name,
+                    gradientColors = subject.colors,
+                    onClick = {}
+                )
+            }
+        }
+    }
+}
